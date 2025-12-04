05c2c7b61f49947d84e7e57cc3fc15fffc7ccb6f
bablyraj <priyagupta82444@gmail.com>
2025-12-04 08:56:34 +0530

    add about us page route

--- frontend.py
+++ frontend.py
@@ -23,6 +23,10 @@ def signup():
 def contact():
     return render_template("contact.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
